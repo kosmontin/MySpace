d103184332bf9b2cd9ceaffe51d70a4264c0e6ca
Konstantin Beloborodov <kosta.white@gmail.com>
2022-02-11 18:48:12 +0300

    download image from net and put into folder

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import os
+
 import requests
 
+images_dir = 'images'
 response = requests.get('https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg')
-with open('hubble.jpeg', 'wb') as file:
+os.makedirs(images_dir, exist_ok=True)
+with open(os.path.join(images_dir, 'hubble.jpeg'), 'wb') as file:
     file.write(response.content)
