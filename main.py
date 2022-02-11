c30210020e125744a5d5560b55b32cf928785640
Konstantin Beloborodov <kosta.white@gmail.com>
2022-02-11 18:55:33 +0300

    add download_image function

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ import os
 
 import requests
 
-images_dir = 'images'
-response = requests.get('https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg')
-os.makedirs(images_dir, exist_ok=True)
-with open(os.path.join(images_dir, 'hubble.jpeg'), 'wb') as file:
-    file.write(response.content)
+
+def download_image(url, path='images'):
+    response = requests.get(url)
+    response.raise_for_status()
+    os.makedirs(path, exist_ok=True)
+    with open(os.path.join(path, 'hubble.jpeg'), 'wb') as file:
+        file.write(response.content)
+
+
+def main():
+    download_image('https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg')
+
+
+if __name__ == '__main__':
+    main()
