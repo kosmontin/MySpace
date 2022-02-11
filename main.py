8bdf85ad5171c5f9dc42dd255a2a221161653f09
Konstantin Beloborodov <kosta.white@gmail.com>
2022-02-11 19:45:38 +0300

    get image links with start flights to commandline

--- main.py
+++ main.py
@@ -13,6 +13,16 @@ def download_image(url, path='images'):
 
 def main():
     download_image('https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg')
+    params = {
+        'launch_year': 2020,
+        'limit': 3
+    }
+    response = requests.get('https://api.spacexdata.com/v3/launches/', params=params)
+    response.raise_for_status()
+    flights = response.json()
+    if flights:
+        for flight in flights:
+            print(*flight['links']['flickr_images'], sep='\n')
 
 
 if __name__ == '__main__':
