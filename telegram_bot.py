2fb7f81ab2ebc0ae777fab35ed854e4facccfecf
Konstantin Beloborodov <kosta.white@gmail.com>
2022-02-14 19:42:45 +0300

    publish photo to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -9,4 +9,5 @@ CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 text_speech = 'Hello, my friend. My name is Dormammu. Would you talk with me?'
 bot = telegram.Bot(token=API_KEY)
-bot.send_message(text=text_speech, chat_id=CHAT_ID)
+# bot.send_message(text=text_speech, chat_id=CHAT_ID)
+bot.send_document(chat_id=CHAT_ID, document=open('images/NASA/skylab_nasa.jpg', 'rb'))
